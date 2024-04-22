a734ee98c6046683244445423261dbd20ab831cf
hariharan-c <hariharan.pkc@gmail.com>
2024-04-22 13:33:31 +0530

    tweaks 16 to workflows

--- alpha/__tests__/test-one.test.js
+++ alpha/__tests__/test-one.test.js
@@ -2,5 +2,5 @@ test('alpha test one', () => {
   expect(5*5).toBe(25);
 });
 test('alpha test one', () => {
-  expect(6*6).toBe(37);
+  expect(6*6).toBe(36);
 });
--- beta/__tests__/test-one.text.js
+++ beta/__tests__/test-one.text.js
@@ -2,5 +2,5 @@ test('beta test one', () => {
   expect(5*5).toBe(25);
 });
 test('beta test one', () => {
-  expect(5*5).toBe(28);
+  expect(6*6).toBe(36);
 });
